8ed402e4c1e7cf625b23ee8c326b619d77ea9aa6
Alam Guardin <alamguardin@gmail.com>
2024-03-04 22:22:11 -0500

    feat: create delete project method

--- main.py
+++ main.py
@@ -28,6 +28,15 @@ class Cli:
         if saveIn == 'Projects': self.projects = content
         if saveIn == 'Tasks': self.tasks = content
     
+    def __check_exist_project(self, id):
+        index = 0
+        for project in self.projects:
+            if project['id'] == id:
+                return index, True
+            index += 1
+        
+        return -1, False
+    
     def __check_exits_task(self, id):
         index = 0
         for task in self.tasks:
@@ -36,6 +45,7 @@ class Cli:
             index += 1
         
         return -1, False
+    
 
     # Public Methods
     def add_command(self, command):
@@ -52,7 +62,7 @@ class Cli:
         self.tasks_file = file
         self.__readFile(file, 'Tasks')
     
-    def check_exist_project(self, id):
+    def exist_project(self, id):
         for project in self.projects:
             if project['id'] == id:
                 return True
@@ -101,6 +111,26 @@ class Cli:
             print('This task does not exist, make sure you enter an existing ID')
         
         self.__writeFile(self.tasks_file, json.dumps(self.tasks))
+    
+    def delete_project(self, id):
+        index, is_found = self.__check_exist_project(id)
+        if is_found:
+            self.projects.pop(index)
+            
+            tasks = []
+            for task in self.tasks:
+                if task['reference'] != id:
+                    tasks.append(task)
+            
+            self.tasks = tasks
+
+            print('Successful project deletion')
+        else:
+            print('This project does not exist, make sure to enter an existing ID')
+        
+        self.__writeFile(self.projects_file, json.dumps(self.projects))
+        self.__writeFile(self.tasks_file, json.dumps(self.tasks))
+
 
 if __name__ == '__main__':
     cli = Cli()
@@ -130,7 +160,7 @@ if __name__ == '__main__':
     if commands[1] == 'create' and commands[2]  == 'task':
         try:
             id_project = commands[3]
-            if cli.check_exist_project(id_project):
+            if cli.exist_project(id_project):
                 while(True):
                     task_description = input('Write a description for your task -> ')
                     if len(task_description) > 0:
@@ -159,3 +189,10 @@ if __name__ == '__main__':
             cli.update_task_status(id_task)
         except IndexError:
              print('You must add the task id to change its status')
+    
+    if commands[1] == 'delete' and commands[2] == 'project':
+        try:
+            id_project = commands[3]
+            cli.delete_project(id_project)
+        except IndexError:
+            print('You must add an id to delete a task')
